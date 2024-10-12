6e1c545543502b4d9ddad836dfbb283f103fdb16
luismy <miguel.buny@gmail.com>
2024-10-12 01:38:20 -0600

    se agrego el resto de proyectos al portafolio, mañana continuamos. seee

--- views/proyectos/proyectos.php
+++ views/proyectos/proyectos.php
@@ -12,36 +12,129 @@
 
     <section class="color">
             <div class="proyectos">
+               
+            <div class="proyecto">
+                    <div class="proyecto__imagen">
+                        <img src="build/img/devstagram.png" alt="imagen devstagram">
+                    </div>
+
+                <div class="proyecto__descripcion">
+                        <h3 class="proyecto__descripcion-titulo">Devstagram - un clon de instagram pero enfocado a programadores</h3>
+                        <p>Una red social clon de instagram, permite publicar, comentar, administrar y seguir a otros desarrolladores y enterarte de su vida, creado en Laravel 9 con Tailwindcss, livewire y vite usando MVC, POO y el ORM Eloquent para establecer relaciones </p>
+                        <div class="proyecto__descripcion--botones">
+                            <a class="proyecto__descripcion-botonG" href="https://github.com/Luismy-182/Devstagram" target="_blank">Ver en Github</a>    
+                            <a class="proyecto__descripcion-botonP" href="https://wabreca.sao.dom.my.id/login" target="_blank">Ir al proyecto</a>
+                        </div>
+                    </div>
+                </div><!---------fin proyecto---------------------->
+
                 <div class="proyecto">
                     <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="seguimientos veterinaria">
+                        <img src="build/img/gpoalze.png" alt="festival de rock">
+                    </div>
+
+
+                <div class="proyecto__descripcion">
+                        <h3 class="proyecto__descripcion-titulo">Portal de proveedores GrupoAlze</h3>
+                        <p>Aplicación web para administrar todos los proveedores y prospectos del grupo industrial Alze, soporta carga de archivos, envio de emails automaticos desde el portal, roles de usuario entre ellos: prospectos, proveedores, administradores del departamento de compras y gerente de compras con todos los privilegios, recordatorios automaticos para los proveedores, historial de documentación, capcha, validacion y sanitizacion de datos en el servidor
+                            y generación de documentación automatica en pdf, creado con php, css3, composer, phpmailer y javascript usando código espagueti, por motivos de privacidad de la empresa solo puedo adjuntar el código fuente a Github ya que el proyecto se encuentra implementado y en funcionamiento</p>
+                        <div class="proyecto__descripcion--botones">
+                            <a class="proyecto__descripcion-botonG" href="https://github.com/Luismy-182/gpoalze.com/tree/main/compras" target="_blank">Ver en Github</a>    
+                            
+                        </div>
+                    </div>
+                </div><!---------fin proyecto---------------------->
+
+                <div class="proyecto">
+                    <div class="proyecto__imagen">
+                        <img src="build/img/uptask_mvc.png" alt="festival de rock">
+                    </div>
+
+
+                    <div class="proyecto__descripcion">
+                        <h3 class="proyecto__descripcion-titulo">UpTask - una aplicación para gestionar proyectos</h3>
+                        <p>Aplicación web para gestionar proyectos y las tareas necesarias a realizar para completarlos, soporta registros de usuarios, autentificación de cuentas mediante tokens y envio de emails,
+                            creado usando FetchAPI, Tailwindcss, Nodejs, dotenv, composer, php, MariaDB, phpmailer y MVC con POO.
+                            Credenciales si no deseas crear una cuenta: correo@gmail.com y contraseña para ambos: tacosalto-182</p>
+                        <div class="proyecto__descripcion--botones">
+                            <a class="proyecto__descripcion-botonG" href="https://github.com/Luismy-182/UpTask_mvc" target="_blank">Ver en Github</a>    
+                            <a class="proyecto__descripcion-botonP" href="https://riwfaka.sgp.dom.my.id/" target="_blank">Ir al proyecto</a>
+                        </div>
+                    </div>
+                </div><!---------fin proyecto---------------------->
+
+
+                <div class="proyecto">
+                    <div class="proyecto__imagen">
+                        <img src="build/img/appsalon_mvc.png" alt="aplicacion de appsalon">
+                    </div>
+
+                    <div class="proyecto__descripcion">
+                        <h3 class="proyecto__descripcion-titulo">Appsalon barbería</h3>
+                        <p>Aplicación web para administrar servicios y citas de clientes de una barberia, soporta envio de tokens para completar registros de usuarios mediante email, y permite agendar tanto el servicio, tipo de corte y la fecha para reservar tu cita. Y del lado del administrador le permite saber cuantas citas tiene agendadas y el precio a cobrar, 
+                            creado con javascript y fetchAPI (antes AJAX), php, mysql, Tailwindcss, MVC y POO.<br>
+                            Credenciales si no deseas crear una cuenta: correo@gmail.com o correo@hotmail.com y contraseña para ambos: tacosalto-182 </p>
+                        <div class="proyecto__descripcion--botones">
+                            <a class="proyecto__descripcion-botonG" href="https://github.com/Luismy-182/Appsalon_mvc" target="_blank">Ver en Github</a>    
+                            <a class="proyecto__descripcion-botonP" href="https://giywaya.nue.dom.my.id/" target="_blank">Ir al proyecto</a>
+                        </div>
+                    </div>
+                </div><!---------fin proyecto---------------------->
+
+
+
+                <div class="proyecto">
+                    <div class="proyecto__imagen">
+                        <img src="build/img/veterinaria.png" alt="">
+                    </div>
+
+                    <div class="proyecto__descripcion">
+                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
+                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, permite editar y borrar las bitacoras realizado con Javascript y TailwindCSS</p>
+                        <div class="proyecto__descripcion--botones">
+                            <a class="proyecto__descripcion-botonG" href="#" target="_blank">Ver en Github</a>    
+                            <a class="proyecto__descripcion-botonP" href="#" target="_blank">Ir al proyecto</a>
+                        </div>
+                    </div>
+                </div><!---------fin proyecto---------------------->
+
+
+                <div class="proyecto">
+                    <div class="proyecto__imagen">
+                        <img src="build/img/cursos.png" alt="">
+                    </div>
+
+                    <div class="proyecto__descripcion">
+                        <h3 class="proyecto__descripcion-titulo">Universidad - una página web que vende cursos</h3>
+                        <p>Una página web que vende cursos con un carrito de compras y localstorage creado usando Skeleton, css y Javascript con Localstorage
+                            para no perder los productos despues de recargar, tal como lo hace Amazon o Mercado Libre
+                        </p>
+                        <div class="proyecto__descripcion--botones">
+                            <a class="proyecto__descripcion-botonG" href="https://github.com/Luismy-182/universidad_carrito" target="_blank">Ver en Github</a>    
+                            <a class="proyecto__descripcion-botonP" href="https://universidad-cursos.netlify.app/" target="_blank">Ir al proyecto</a>
+                        </div>
+                    </div>
+                </div><!---------fin proyecto---------------------->
+
+
+   
+
+
+                <div class="proyecto">
+                    <div class="proyecto__imagen">
+                        <img src="build/img/veterinaria.png" alt="">
                     </div>
 
                     <div class="proyecto__descripcion">
                         <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
                         <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
                         <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
+                            <a class="proyecto__descripcion-botonG" href="https://github.com/Luismy-182/seguimiento-pacientes-veterinaria" target="_blank">Ver en Github</a>    
+                            <a class="proyecto__descripcion-botonP" href="https://veterinaria-seguimientos.netlify.app/" target="_blank">Ir al proyecto</a>
                         </div>
                     </div>
                 </div><!---------fin proyecto---------------------->
 
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/rockfestival.png" alt="festival de rock">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Rock and Tech festival</h3>
-                        <p>Página web simple de un festival de rock y Techno, con SASS y GULP.JS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
 
 
 
@@ -54,125 +147,12 @@
                         <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
                         <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
                         <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
+                            <a class="proyecto__descripcion-botonG" href="#" target="_blank">Ver en Github</a>    
+                            <a class="proyecto__descripcion-botonP" href="#" target="_blank">Ir al proyecto</a>
                         </div>
                     </div>
                 </div><!---------fin proyecto---------------------->
 
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
-                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
-
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
-                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
-
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
-                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
-
-
-
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
-                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
-
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
-                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
-
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
-                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
-
-
-                <div class="proyecto">
-                    <div class="proyecto__imagen">
-                        <img src="build/img/veterinaria.png" alt="">
-                    </div>
-
-                    <div class="proyecto__descripcion">
-                        <h3 class="proyecto__descripcion-titulo">Seguimiento de pacientes de una veterinaria</h3>
-                        <p>Página web para darle seguimiento a pacientes que fueron dados de alta, realizado con Javascript y TailwindCSS</p>
-                        <div class="proyecto__descripcion--botones">
-                            <a class="proyecto__descripcion-botonG" href="#">Ver en Github</a>    
-                            <a class="proyecto__descripcion-botonP" href="#">Ir al proyecto</a>
-                        </div>
-                    </div>
-                </div><!---------fin proyecto---------------------->
 
             </div>
         </section>
